1cfefce5a23e7a9540be13d82d0b6d98720e747b
gods_type <ricksue99@gmail.com>
2025-08-29 16:40:58 +0800

    netlifyfix

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/components/theme-provider";
+import "./global.css";
+import { ThemeProvider } from "@/componets/theme-provider";
 
 const geist = Geist({
   variable: "--font-geist-sans",
